5046ab13a8f3d60b9cd34baa6836b5a132812ed6
juanmafc <juanmafc25@gmail.com>
2021-02-06 18:22:49 -0300

    Added YoutubePlaylistVideosResponse class

---
+++ public/YoutubeAPI/YoutubePlaylistVideosResponse.ts
@@ -0,0 +1,29 @@
+import {YoutubeVideo} from "./YoutubeSchemas";
+import {YoutubePageToken} from "./YoutubePageToken";
+import {youtube_v3} from "googleapis";
+import Schema$PlaylistItemListResponse = youtube_v3.Schema$PlaylistItemListResponse;
+
+export class YoutubePlaylistVideosResponse {
+    constructor(private response: Schema$PlaylistItemListResponse) {
+    };
+
+    public getVideos() {
+        let videos: YoutubeVideo[] = []
+        let responseVideos = this.response.items;
+        if (responseVideos !== undefined) {
+            videos = responseVideos
+                .filter(video => video.id !== null && video.snippet !== undefined).map(video => {
+                    return {
+                        id: <string>video.id,
+                        title: <string>video.snippet!.title
+                    };
+                });
+        }
+        return videos
+    }
+
+    public getNextPageToken() {
+        let nextPageToken = this.response.nextPageToken !== null ? this.response.nextPageToken : undefined;
+        return new YoutubePageToken(nextPageToken);
+    }
+}
